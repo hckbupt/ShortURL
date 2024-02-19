53494fef155723077415adf7a81a107121e19d3a
hck <522625603@qq.com>
2024-02-19 19:49:42 +0800

    测试push是否正常

--- src/main/java/top/naccl/dwz/DwzApplication.java
+++ src/main/java/top/naccl/dwz/DwzApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 public class DwzApplication {
 
+	//测试
 	public static void main(String[] args) {
 		SpringApplication.run(DwzApplication.class, args);
 	}
